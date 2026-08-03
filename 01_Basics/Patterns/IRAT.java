bf0b0dec7eb9160cd1c56ad82b5aa6e6ff7befb5
R Eswar <sanjayeswar444@gmail.com>
2026-08-03 20:50:07 +0530

    added inverted right angled triangle

--- 01_Basics/Patterns/IRAT.java
+++ 01_Basics/Patterns/IRAT.java
@@ -1,11 +1,12 @@
-public class RAT {
-    public static void main(String[] args){
+public class IRAT {
+    public static void main(String[] args) {
         int n = 5;
         for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= i; j++){
+            for(int j = 1; j <= n-i+1; j++){
                 System.out.print("*");
             }
             System.out.println();
         }
     }
+    
 }
