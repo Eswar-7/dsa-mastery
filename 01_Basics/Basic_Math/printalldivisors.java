71c37074e087b3a9b2172f2ca1e21d16d2b50874
R Eswar <sanjayeswar444@gmail.com>
2026-08-08 18:21:28 +0530

    Added print all divisors program

---
+++ 01_Basics/Basic_Math/printalldivisors.java
@@ -0,0 +1,14 @@
+public class printalldivisors {
+    public static void main(String[] args) {
+        int n = 24;
+        for(int i = 1; i <= Math.sqrt(n); i++){
+            if(n % i == 0){
+                System.out.println(i);
+                   if (i != n / i) {
+                    System.out.println(n / i);
+                }
+            }
+        }
+    }
+    
+}
